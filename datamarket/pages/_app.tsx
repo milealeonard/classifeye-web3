b52e85ca4542d2fdbdbc9f34e061077dfef9652d
Leonard <milealeonard@gmail.com>
2024-06-17 18:55:57 -0500

    add toaster (not sure if this the right way)

--- datamarket/pages/_app.tsx
+++ datamarket/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col justify-center items-center w-screen">
       <Component {...pageProps} />
+      <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
 }
